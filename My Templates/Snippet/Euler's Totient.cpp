e32a6780a24f22b7e3675e8c68470d81aa462df3
AHNAF SHAHRIAR <148708967+Ahnaf-2210007@users.noreply.github.com>
2025-01-18 23:56:49 +0600

    Create Euler's Totient.cpp

---
+++ My Templates/Snippet/Euler's Totient.cpp
@@ -0,0 +1,20 @@
+```cpp
+const int MAX = 5000000;
+
+vector<ull> phi(MAX + 1);
+
+void computeTotient() {
+    for (int i = 0; i <= MAX; i++) {
+        phi[i] = i;  // Initialize phi[i] to i
+    }
+
+    for (int i = 2; i <= MAX; i++) {
+        if (phi[i] == i) {  // i is prime
+            for (int j = i; j <= MAX; j += i) {
+                phi[j] *= (i - 1);
+                phi[j] /= i;
+            }
+        }
+    }
+}
+```
